2a1207ccc68b434bab2e394db117bf6afbbdd182
BobPilott <bobpilott@gmail.com>
2019-02-05 18:25:06 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo4';
+
+  @Output('show') showEvent = new EventEmitter();
+
+  contacts:Contact[]=[];
+
+  currContact:Contact|null = null;
+  currIndex = -1;
+
+  removeContact(i: number):void {
+    this.contacts.splice(i, 1);
+    if(this.currIndex === i){
+      this.currContact = null;
+    }
+  }
+
+  showContact(i: number, contact: Contact) {
+    this.currIndex = i;
+    this.currContact = contact;
+  }
+
+  addContact(cont: Contact) {
+    this.contacts.push(cont);
+  }
 }
+
+export type Contact={
+  name:string;
+  email:string;
+  phone:string;
+  address:string;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
